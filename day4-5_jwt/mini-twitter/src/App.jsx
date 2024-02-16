e253fb44e69f500990c5bd093afab6dcf49303af
totycos <dv94@hotmail.fr>
2024-02-16 18:04:05 +0100

    Commit REACT_ADVANCE day 5 - mini-twitter: add private route on profile

--- day4-5_jwt/mini-twitter/src/App.jsx
+++ day4-5_jwt/mini-twitter/src/App.jsx
@@ -1,28 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/Navbar";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
 import "./App.scss";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/profile" element={<ProfilePage />} exact />
+          </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
